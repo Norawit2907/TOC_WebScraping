ed01164a6f2b57e59749413d22758e2839a6fac3
o0XDuser0o <secretdata3351@gmail.com>
2024-09-08 01:04:41 +0700

    feat(func): Crawling path game review

--- flask_app/crawling.py
+++ flask_app/crawling.py
@@ -5,16 +5,38 @@ import re, requests
 
 def crawling():
     #start->
-    resp = requests.get("https://www.eurogamer.net/archive/2024/09")
+    listOfLink = []
+    starturl = '/archive/2024/09'
+    path = "https://www.eurogamer.net"
+
+    resp = requests.get(f"{path}{starturl}")
     resp = resp.text
 
-    testStr = 'dhaslkdjasldjsdjhref="/archive/24747/572"dasdsadsaddsdsadas'
+    testStr = 'dhaslkdjasldjsdj<a href="https://www.eurogamer.net/sd-asda-review-das-dsa">sdddasdsadsaddsdsadas'
 
     #regular expression
-    listOfLink = re.findall(r'href="/archive/[0-9]*/[0-9]*"', resp)
+    listOfmonth = re.findall(r'<a href="/archive/[0-9]*/[0-9]*"', resp)
+
+    # resp = requests.get(f"{path}{listOfmonth[0][9:-1]}")
+    # resp = resp.text
+    # lst = re.findall(r'href="https://www.eurogamer.net/.*-review.*"',resp)
+    # for l in lst:
+    #     print(l)
+
+    print(len(listOfmonth))
+    for link in listOfmonth:
+        resp = requests.get(f"{path}{link[9:-1]}")
+        resp = resp.text
+        lst = re.findall(r'href="https://www.eurogamer.net/.*-review"',resp)
+        listOfLink.extend(lst)
+        print(link, "success", len(lst))
+    
     print(len(listOfLink))
     for link in listOfLink:
         print(link)
+
+    # lst = re.findall(r'<a href="https://www.eurogamer.net/.*-review.*">',testStr)
+    # print(lst)
 
 print("Start")
 crawling()
