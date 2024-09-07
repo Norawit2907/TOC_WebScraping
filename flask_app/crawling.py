08a6960be06bc21aad35b5f1ffeae5ae57911a74
o0XDuser0o <secretdata3351@gmail.com>
2024-09-07 21:42:33 +0700

    feat(regEx): crawl month page

--- flask_app/crawling.py
+++ flask_app/crawling.py
@@ -1,5 +1,21 @@
-import re
+import re, requests
+
+#https://store.steampowered.com/search/?sort_by=Released_DESC&os=win (infinite scroll problem)
+#https://www.eurogamer.net/reviews
 
 def crawling():
-    pass
+    #start->
+    resp = requests.get("https://www.eurogamer.net/archive/2024/09")
+    resp = resp.text
 
+    testStr = 'dhaslkdjasldjsdjhref="/archive/24747/572"dasdsadsaddsdsadas'
+
+    #regular expression
+    listOfLink = re.findall(r'href="/archive/[0-9]*/[0-9]*"', resp)
+    print(len(listOfLink))
+    for link in listOfLink:
+        print(link)
+
+print("Start")
+crawling()
+print("End")
