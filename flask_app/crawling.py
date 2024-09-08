1989a7dd277e21fab604714d64976491f3f61ba4
o0XDuser0o <secretdata3351@gmail.com>
2024-09-08 14:08:49 +0700

    feat(RegEx): Limit amount of crawling page

--- flask_app/crawling.py
+++ flask_app/crawling.py
@@ -1,42 +1,45 @@
 import re, requests
 
-#https://store.steampowered.com/search/?sort_by=Released_DESC&os=win (infinite scroll problem)
+## Path ref ##
+#https://www.eurogamer.net/archive/YYYY/MM
 #https://www.eurogamer.net/reviews
 
 def crawling():
-    #start->
     listOfLink = []
-    starturl = '/archive/2024/09'
-    path = "https://www.eurogamer.net"
+    startPath = '/archive/2024/09'
+    basePath = "https://www.eurogamer.net"
 
-    resp = requests.get(f"{path}{starturl}")
+    resp = requests.get(f"{basePath}{startPath}")
     resp = resp.text
 
-    testStr = 'dhaslkdjasldjsdj<a href="https://www.eurogamer.net/sd-asda-review-das-dsa">sdddasdsadsaddsdsadas'
+    ## Test Regular Expression ##
+    # testStr = 'dhaslkdjasldjsdj<a href="https://www.eurogamer.net/sd-asda-review-das-dsa">sdddasdsadsaddsdsadas'
+    # lst = re.findall(r'<a href="https://www.eurogamer.net/.*-review.*">',testStr)
+    # print(lst)
 
-    #regular expression
     listOfmonth = re.findall(r'<a href="/archive/[0-9]*/[0-9]*"', resp)
+    print(len(listOfmonth))
 
-    # resp = requests.get(f"{path}{listOfmonth[0][9:-1]}")
+    ## Test Only One Page ##
+    # resp = requests.get(f"{basePath}{listOfmonth[0][9:-1]}")
     # resp = resp.text
     # lst = re.findall(r'href="https://www.eurogamer.net/.*-review.*"',resp)
     # for l in lst:
     #     print(l)
 
-    print(len(listOfmonth))
     for link in listOfmonth:
-        resp = requests.get(f"{path}{link[9:-1]}")
+        resp = requests.get(f"{basePath}{link[9:-1]}")
         resp = resp.text
-        lst = re.findall(r'href="https://www.eurogamer.net/.*-review"',resp)
+        lst = re.findall(r'href="https://www.eurogamer.net/.*-review"',resp) #digitalfoudry should not exist
         listOfLink.extend(lst)
         print(link, "success", len(lst))
+        if len(listOfLink) > 400:
+            break
     
     print(len(listOfLink))
     for link in listOfLink:
         print(link)
 
-    # lst = re.findall(r'<a href="https://www.eurogamer.net/.*-review.*">',testStr)
-    # print(lst)
 
 print("Start")
 crawling()
