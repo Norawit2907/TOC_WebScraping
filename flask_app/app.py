67c492589639fe365c85b0844e0e7c1b519d4925
tatanoi <ta.phasin@gmail.com>
2024-09-26 23:36:31 +0700

    feat : loop game_list

--- flask_app/app.py
+++ flask_app/app.py
@@ -37,7 +37,8 @@ def crawl():
         games = [game for game in games if search_query in game[0].lower()]
         
     game_list = [{'game': game[0], 'alphabet': game[1], 'release_date': game[2]} for game in games]    
-    return render_template('index.html', ins = game_list)
+    return render_template('table.html', game=game_list)
+
   
 if __name__ == "__main__":
     app.run(debug=True)
