dda47920d03e6d6eade5622fe16c68708f6c7a5c
tatanoi <ta.phasin@gmail.com>
2024-09-26 23:15:58 +0700

    feat : test append to html

--- flask_app/app.py
+++ flask_app/app.py
@@ -37,7 +37,7 @@ def crawl():
         games = [game for game in games if search_query in game[0].lower()]
         
     game_list = [{'game': game[0], 'alphabet': game[1], 'release_date': game[2]} for game in games]    
-    return game_list
+    return render_template('index.html', ins = game_list)
   
 if __name__ == "__main__":
     app.run(debug=True)
